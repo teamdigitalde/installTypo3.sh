0c119a82c93a1102e454560ca3917f91f9d960e3
Tim Büschken <bueschken@team-digital.de>
2018-10-15 08:07:13 +0200

    [UPDATE] Delete index.php Part

--- installTypo3.sh
+++ installTypo3.sh
@@ -454,35 +454,6 @@ printf 'beta:$1$$.OPcLRctp0tpQ81Db9tKP/' >> .htpasswd
 #change to typo3 directory
 cd typo3/
 
-#create index.php
-#ln -s typo3_src/index.php index.php
-touch index.php
-printf "<?php
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
-// Exit early if php requirement is not satisfied.
-if (PHP_VERSION_ID < 70200) {
-    die('This version of TYPO3 CMS requires PHP 7.2 or above');
-}
-
-// Set up the application for the frontend
-call_user_func(function () {
-    $classLoader = require 'typo3_src/vendor/autoload.php';
-    \TYPO3\CMS\Core\Core\SystemEnvironmentBuilder::run(0, \TYPO3\CMS\Core\Core\SystemEnvironmentBuilder::REQUESTTYPE_FE);
-    \TYPO3\CMS\Core\Core\Bootstrap::init($classLoader)->get(\TYPO3\CMS\Frontend\Http\Application::class)->run();
-});" >> index.php
-
 #create fileadmin, user_upload and typo3conf
 mkdir fileadmin
 cd fileadmin
