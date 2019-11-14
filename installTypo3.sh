2fcee78bc2abf9ad68593faaa7d47e3bf2498175
Tim Büschken <bueschken@team-digital.de>
2019-11-14 13:25:30 +0100

    [TEST] Für 9.2 Flux Version

--- installTypo3.sh
+++ installTypo3.sh
@@ -339,7 +339,7 @@ cd ../
 mkdir flux
 cd flux
 git init > /dev/null
-git pull https://github.com/FluidTYPO3/flux/tree/master > /dev/null 2>&1
+git pull https://github.com/FluidTYPO3/flux/tree/9.2.0 > /dev/null 2>&1
 
 cd ../
 mkdir vhs
